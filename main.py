b75eb97321703fa387caac2efb1f401dadabf9ce
abigu73 <151196605+abigu73@users.noreply.github.com>
2024-02-03 01:22:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+from telegram import Bot
+import logging
+import time
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+def scraping_data():
+    url = 'https: http:/www.voanews.com/'
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        titles = soup.find_all('h2', class_='post-title')
+        news_data = [title.text.strip() for title in titles]
+        return '\n'.join(news_data)
+    else:
+        logging.error("Failed to fetch data from the website.")
+        return "Failed to fetch data from the website."
+
+def send_to_telegram_bot(bot_token, chat_id, data):
+    bot = Bot(token=bot_token)
+    try:
+        bot.send_message(chat_id=chat_id, text=data)
+        logging.info("Sent data to Telegram bot.")
+    except Exception as e:
+        logging.error(f"Failed to send data to Telegram bot: {e}")
+    
+def main():
+    BOT_TOKEN = '6363494971:AAHfGd4rJqWTnPGqf4PhnNEUkFwoc1rIOxc'
+    BOT_CHAT_ID = '1602509372'
+    def send_to_telegram(bot_token, chat_id, text):
+     url = f"https://t.me/aywlm_bot{bot_token}/sendMessage"
+    
+    while True:
+        data = scraping_data()
+        try:
+            send_to_telegram_bot(BOT_TOKEN, BOT_CHAT_ID, data)
+        except Exception as e:
+            logging.error(f"An error occurred after sending data to Telegram bot: {e}")
+
+        time.sleep(3600)
+
+if __name__ == '__main__':
+   main()
